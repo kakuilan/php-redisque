3fcee3696ee39090e5b14eb1e5dadd6cf96b688d
kakuilan <kakuilan@163.com>
2020-07-18 20:22:16 +0800

    add RedisQueue

---
+++ src/RedisQueue.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/7/18
+ * Time: 20:04
+ * Desc: redis队列
+ */
+
+namespace Redisque;
+
+use Kph\Services\BaseService;
+
+class RedisQueue extends BaseService {
+
+}
