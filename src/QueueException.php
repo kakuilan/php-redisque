a404d0bc73a1ae2630918c20e37325c697640385
kakuilan <kakuilan@163.com>
2020-07-23 09:27:46 +0800

    add QueueException

---
+++ src/QueueException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/7/18
+ * Time: 21:40
+ * Desc: 队列异常
+ */
+
+namespace Redisque;
+
+use Kph\Exceptions\BaseException;
+
+class QueueException extends BaseException {
+
+}
--- src/RedisQueue.php
+++ src/RedisQueue.php
@@ -10,7 +10,26 @@
 namespace Redisque;
 
 use Kph\Services\BaseService;
+use Kph\Consts;
+use Redis;
 
 class RedisQueue extends BaseService {
+
+
+    /**
+     * 默认配置
+     * @var array
+     */
+    public static $defaultConf = [
+        'host'         => '127.0.0.1',
+        'port'         => 6379,
+        'password'     => null,
+        'select'       => 0,
+        'wait_timeout' => Consts::TTL_TWO_MINUTE,
+    ];
+
+
+
+
 
 }
