ca46175065dbe3ed3d70f179c8540b9b24cc07ff
kakuilan <kakuilan@163.com>
2020-07-28 17:48:22 +0800

    fix RedisQueue

--- src/RedisQueue.php
+++ src/RedisQueue.php
@@ -14,6 +14,7 @@ use Kph\Consts;
 use Kph\Helpers\ArrayHelper;
 use Kph\Helpers\ValidateHelper;
 use Redis;
+use RedisException;
 use Throwable;
 use Error;
 
@@ -25,6 +26,20 @@ class RedisQueue extends BaseService {
 
 
     /**
+     * 队列相关键前缀
+     * @var string
+     */
+    protected $prefix = 'que_';
+
+
+    /**
+     * redis配置
+     * @var array
+     */
+    protected $conf = [];
+
+
+    /**
      * 默认配置
      * @var array
      */
@@ -33,7 +48,6 @@ class RedisQueue extends BaseService {
         'port'         => 6379,
         'password'     => null,
         'database'     => 0,
-        'prefix'       => 'que_', //key前缀
         'wait_timeout' => Consts::TTL_TWO_MINUTE,
     ];
 
@@ -52,6 +66,12 @@ class RedisQueue extends BaseService {
     public static $clients = [];
 
 
+    /**
+     * 根据配置获取redis客户端
+     * @param array $conf
+     * @return Redis
+     * @throws QueueException
+     */
     public static function getRedisByConf(array $conf = []): Redis {
         if (empty($conf)) {
             $conf = self::$defaultConf;
@@ -98,9 +118,8 @@ class RedisQueue extends BaseService {
                 $ret = $redis->auth($conf['password']);
             }
 
-            if(!$ret) {
-                $msg = QueueException::ERR_CONF_MSG . implode(',', array_keys(self::$defaultConf)) . '.';
-                throw new QueueException($msg, QueueException::ERR_CONF_CODE);
+            if (!$ret) {
+                throw new QueueException(QueueException::ERR_CANNOT_CONNECT_MSG, QueueException::ERR_CANNOT_CONNECT_CODE);
             }
 
             $redis->select($conf['database']);
@@ -113,21 +132,90 @@ class RedisQueue extends BaseService {
             self::$clients[$clientKey] = $connInfo;
         }
 
+        return $connInfo['redis'];
     }
 
 
+    /**
+     * 根据键获取redis客户端
+     * @param string $key 客户端标识键
+     * @return Redis
+     * @throws QueueException
+     */
     public static function getRedisByKey(string $key = ''): Redis {
+        if (empty($key) || !isset(self::$clients[$key])) {
+            throw new QueueException(QueueException::ERR_CLIENT_NOTEXIST_MSG, QueueException::ERR_CLIENT_NOTEXIST_CODE);
+        }
 
+        $connInfo      = self::$clients[$key];
+        $now           = time();
+        $socketTimeout = ini_get('default_socket_timeout');
+        $waitTimeout   = intval(self::$defaultConf['wait_timeout']);
+        if ($socketTimeout > 0) {
+            $waitTimeout = min($socketTimeout, $waitTimeout);
+        }
+
+        $lastTime   = $connInfo['last_connect_time'] ?? 0; //上次连接时间
+        $expireTime = $lastTime + $waitTimeout; //预计过期时间
+
+        if (!($now >= $lastTime && $now < $expireTime)) {
+            $pingRes = false;
+            try {
+                $ping    = $connInfo['redis']->ping();
+                $pingRes = (strpos($ping, "PONG") !== false);
+            } catch (Error $e) {
+            }
+
+            if (!$pingRes) {
+                $msg = QueueException::ERR_CANNOT_CONNECT_MSG . $e->getMessage();
+                throw new QueueException($msg, QueueException::ERR_CANNOT_CONNECT_CODE);
+            }
+
+            self::$clients[$key]['last_connect_time'] = $now;
+        }
+
+        return $connInfo['redis'];
     }
 
 
+    /**
+     * 设置redis客户端对象
+     * @param string $clientKey 客户端标识键
+     * @param Redis $redis 客户端对象
+     * @return bool
+     */
     public function setRedis(string $clientKey, Redis $redis): bool {
+        try {
+            $ping = $redis->ping();
+            $res  = strpos($ping, "PONG") !== false;
+        } catch (Throwable $e) {
+            $res = false;
+        }
 
+        if ($res) {
+            $connInfo = [
+                'redis'             => $redis,
+                'last_connect_time' => time(),
+            ];
+
+            self::$clients[$clientKey] = $connInfo;
+        }
+
+        return $res;
     }
 
 
     public function getRedis($param = null): Redis {
 
+    }
+
+
+    /**
+     * 设置前缀
+     * @param string $str
+     */
+    public function setPrefix(string $str) {
+        $this->prefix = $str;
     }
 
 
