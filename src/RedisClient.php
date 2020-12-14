575dd2820e1b11cfbbd93f1114c40d6e2477154c
kakuilan <kakuilan@163.com>
2020-12-14 11:46:11 +0800

    add RedisClient

---
+++ src/RedisClient.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/12/14
+ * Time: 11:43
+ * Desc:
+ */
+
+namespace Redisque;
+
+use Kph\Services\BaseService;
+
+class RedisClient extends BaseService {
+
+}
