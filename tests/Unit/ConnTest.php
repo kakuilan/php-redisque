a87c6098dfa7615a307ac98f1f49042218e3397d
kakuilan <kakuilan@163.com>
2021-01-21 19:06:29 +0800

    add testGetRedis

--- tests/Unit/ConnTest.php
+++ tests/Unit/ConnTest.php
@@ -12,7 +12,9 @@ namespace Redisque\Tests\Unit;
 use PHPUnit\Framework\TestCase;
 use Error;
 use Exception;
+use Throwable;
 use Redisque\RedisConn;
+use Redisque\RedisClient;
 
 
 /**
@@ -27,8 +29,8 @@ class ConnTest extends TestCase {
      */
     public static $conf = [
         'host'     => '192.168.56.1',
-        'port'     => 6379,
         'password' => '123456',
+        'port'     => 6379,
         'select'   => 1,
     ];
 
@@ -38,6 +40,23 @@ class ConnTest extends TestCase {
 
         $conf = RedisConn::getDefaultConf();
         $this->assertEquals($conf['password'], self::$conf['password']);
+    }
+
+
+    public function testGetRedis() {
+        try {
+            $client = RedisConn::getRedis(self::$conf);
+            $info   = $client->info();
+            $this->assertTrue(is_array($info));
+            $this->assertTrue($client instanceof RedisClient);
+
+            /* @var $client2 RedisClient */
+            $client2 = RedisConn::getRedis(self::$conf);
+            $time1   = $client->getLastConnectTime();
+            $time2   = $client2->getLastConnectTime();
+            $this->assertEquals($time1, $time2);
+        } catch (Throwable $e) {
+        }
     }
 
 
